42573a805840f4b32e5af20b1bd11fe2be1b1036
lardbucket <jaimetropezado@yahoo.com>
2016-04-29 02:53:36 +0800

    Added gray/binary webcam display

--- Webcam/src/MyFrame.java
+++ Webcam/src/MyFrame.java
@@ -1,9 +1,17 @@
 //test
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 public class MyFrame extends JFrame {
     private JPanel contentPane;
@@ -13,12 +21,16 @@ public class MyFrame extends JFrame {
   */
     public static void main(String[] args) 
     {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
+        EventQueue.invokeLater(new Runnable() 
+        {
+            public void run() 
+            {
+                try 
+                {
                     MyFrame frame = new MyFrame();
                     frame.setVisible(true);
-                } catch (Exception e) {
+                } catch (Exception e)
+                {
                     e.printStackTrace();
                 }
             }
@@ -44,12 +56,46 @@ public class MyFrame extends JFrame {
     public void paint(Graphics g)
     {
         g = contentPane.getGraphics();
-        g.drawImage(videoCap.getOneFrame(), 0, 0, this);
+        BufferedImage src = videoCap.getOneFrame();
+        byte[] data = ((DataBufferByte) src.getRaster().getDataBuffer()).getData();
+        Mat mat = new Mat(src.getHeight(), src.getWidth(), CvType.CV_8UC3);
+        mat.put(0, 0, data);
+        Imgcodecs.imwrite("C:/WebcamTest/test2.jpg", mat);
+        mat = Imgcodecs.imread("C:/WebcamTest/test2.jpg");
+        Mat grayMat = new Mat(mat.height(),mat.width(), CvType.CV_8UC1);
+		Imgproc.cvtColor(mat, grayMat, Imgproc.COLOR_RGB2GRAY);
+        Mat binMat = new Mat(mat.height(),mat.width(), CvType.CV_8UC1);
+		Imgproc.threshold(grayMat, binMat, 100, 255, Imgproc.THRESH_BINARY);
+        
+        g.drawImage(binMatToImage(binMat), 0, 0, this);
+        
         //edit here
     }
     
-    
- 
+    public static BufferedImage grayMatToImage(Mat m)
+	 {
+		 byte[] data2 = new byte[m.rows() * m.cols() * (int)(m.elemSize())];
+		 m.get(0, 0, data2);
+		 BufferedImage image1 = new BufferedImage(m.cols(),m.rows(), BufferedImage.TYPE_BYTE_GRAY);
+		 image1.getRaster().setDataElements(0, 0, m.cols(), m.rows(), data2);
+		 return image1;
+	 }
+    public static BufferedImage colorMatToImage(Mat m)
+	 {
+		 byte[] data2 = new byte[m.rows() * m.cols() * (int)(m.elemSize())];
+		 m.get(0, 0, data2);
+		 BufferedImage image1 = new BufferedImage(m.cols(),m.rows(), BufferedImage.TYPE_3BYTE_BGR);
+		 image1.getRaster().setDataElements(0, 0, m.cols(), m.rows(), data2);
+		 return image1;
+	 }
+    public static BufferedImage binMatToImage(Mat m)
+	 {
+		 byte[] data2 = new byte[m.rows() * m.cols() * (int)(m.elemSize())];
+		 m.get(0, 0, data2);
+		 BufferedImage image1 = new BufferedImage(m.cols(),m.rows(), BufferedImage.TYPE_BYTE_BINARY);
+		 image1.getRaster().setDataElements(0, 0, m.cols(), m.rows(), data2);
+		 return image1;
+	 }
     class MyThread extends Thread{
         @Override
         public void run() 
